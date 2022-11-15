9b0f206b3325866fa03ad2045a3e593f9e851417
dharshan1007 <81615110+dharshan1007@users.noreply.github.com>
2022-11-15 11:20:39 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,8 +5,8 @@ terraform {
    bucket         = "terrabuck1411"
    key            = "dev/terraform.tfstate"
    region         = "ap-southeast-1"
-  # profile = "foxconn"
    access_key = "AKIAS3KL5KTN5RCGKNGP"
    secret_key = "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"
-  }
+  # profile = "foxconn"
+   }
 }
