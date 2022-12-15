f4ab7e2519fb0856363e4e00143d72ed836e9c59
dharshan1007 <81615110+dharshan1007@users.noreply.github.com>
2022-12-15 16:26:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   required_version = ">=0.12.6"
   backend "s3" {
-   bucket         = "terrabuck2211"
+   bucket         = "terrabuck1512"
    key            = "dev/terraform.tfstate"
    region         = "ap-southeast-1"
    
